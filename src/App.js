8caf27cfbc4b02a52b12eaa0d1172919b45c9e7b
Lana Londah <S22310372@student.unklab.ac.id>
2024-11-07 10:21:44 +0800

    complete excersze

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./component/Header/Header";
+import Hero from "./component/Hero/hero";
+import Client from "./component/Clients/Clients";
+import Feature  from "./component/Features/Features";
+import Pricing from "./component/Pricing/Pricing";
+import Footer from "./component/Footer/Footer";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="body-wrap">
+      <Header />
+  <main className="site-content">
+    <Hero />
+    <Client />
+    <Feature />
+    <Pricing />
+  </main>
+  <Footer />
+</div>
+
   );
 }
 
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
